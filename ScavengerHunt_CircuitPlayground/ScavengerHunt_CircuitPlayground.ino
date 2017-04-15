37ee1b5795a8d65c3dce46f97d1d96236cbb42ef
goodmj <matthew.j.good@gmail.com>
2017-04-15 16:16:31 -0600

    Initial commit
    Has placeholders for the sensor functions!

---
+++ ScavengerHunt_CircuitPlayground/ScavengerHunt_CircuitPlayground.ino
@@ -0,0 +1,38 @@
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+//  Serial.print("Hello World!");
+}
+
+function colorSense()
+{
+  // if red is > green && red > blue
+  {
+    Serial.print("You found a red object! Congrats!");
+  }
+}
+
+function senseDark()
+{
+  // storage room
+}
+
+function senseLight()
+{
+  // outdoors  
+}
+
+function soundSense()
+{
+  // make a loud noise
+}
+
+function temperatureSense()
+{
+  // go somewhere cold (and will have next clue there)
+  // how about hot?
+}
+
